fffc7c17fe3e054823c3594ef9c5c1aeb8b746ef
Baharan Nouri <baharannouri@Baharans-MacBook-Air.local>
2022-10-16 09:34:07 -0400

    Change model

--- budgetproject/budget/models.py
+++ budgetproject/budget/models.py
@@ -3,15 +3,14 @@ from django.utils.text import slugify
 
 
 class Project(models.Model):
+
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100, unique=True, blank=True)
     budget = models.IntegerField()
 
-
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super(Project, self).save(*args, **kwargs)
-
 
     @property
     def budget_left(self):
@@ -22,7 +21,6 @@ class Project(models.Model):
 
         # temporary solution, because the form currently only allows integer amounts
         total_expense_amount = int(total_expense_amount)
-
         return self.budget - total_expense_amount
 
 
@@ -31,16 +29,13 @@ class Project(models.Model):
         expense_list = Expense.objects.filter(project=self)
         return len(expense_list)
 
+
     def get_absolute_url(self):
         return '/' + self.slug
-
-
-
 
 class Category(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
-
 
 
 class Expense(models.Model):
@@ -48,7 +43,6 @@ class Expense(models.Model):
     title = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=8, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-
 
     class Meta:
         ordering = ('-amount',)
